0ff680f2b5d5b5d55647f92f83a4566e58fd312f
Timothy Zhou <z.timothy96@gmail.com>
2023-12-27 17:15:55 -0800

    plot some brains

--- mycode/get_img_data.py
+++ mycode/get_img_data.py
@@ -1,4 +1,6 @@
 import nibabel as nib
+from nilearn import plotting
+
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
@@ -8,14 +10,16 @@ from sklearn.preprocessing import OneHotEncoder
 scaler = MinMaxScaler()
 encoder = OneHotEncoder(categories='auto', sparse=False)
 
-msk_list=sorted(glob.glob("msk/*msk.nii"))
+msk_list=sorted(glob.glob("NIFTIs_Masks/*msk.nii"))
+img_list=sorted(glob.glob("NIFTIs_Images/*img.nii"))
 saved_values = []
 max_nb_values = 0
 
+def show_img(vol):
+    plotting.plot_img(vol)
+    plt.show()
 
-for i in range(len(msk_list)):
-    msk_name =  msk_list[i]
-    msk_data=nib.load(msk_name).get_fdata()
+def save_img(msk_data):
     msk_data[msk_data%10==1] = 1
     msk_data[msk_data%10==2] = 2
     msk_data[msk_data%10==0] = 0
@@ -25,9 +29,10 @@ for i in range(len(msk_list)):
     print(f"save to {save_name}")
     np.save(save_name, msk_data)
     
-    #plt.figure(figsize=(12, 8))
-
-    #plt.subplot(221)
-    #plt.imshow(msk_data[:,:,64], cmap='gray')
-    #plt.title('Image flair')
-    #plt.show()
+for fname in (img_list[:3]):
+    print('HI')
+    print(fname)
+    vol=nib.load(fname)
+    show_img(vol)
+    data=vol.get_fdata()
+    # save_img(data)
